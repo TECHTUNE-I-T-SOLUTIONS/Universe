28504e62f938a1f86e119c809e7eada19bed8211
TechTune I. T. Solutions <techtune.it.solutions@gmail.com>
2025-01-28 16:20:06 +0100

    continous codes

--- universeapp/src/AdminDashboard/Dashboard/Dashboard.js
+++ universeapp/src/AdminDashboard/Dashboard/Dashboard.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import Analytics from "./Analytics";
 import Reports from "./Reports";
+import CloseIcon from "../../icons/close.png";
 
-const Dashboard = ({ onClose }) => {
+const Dashboard = ({ onClose, showLogoutDialog }) => {
   const [activeTab, setActiveTab] = useState("analytics"); // State to switch between Analytics and Reports
 
   const formatDate = () => {
@@ -11,20 +12,29 @@ const Dashboard = ({ onClose }) => {
   };
 
   return (
-    <div className="academic-progress h-full w-full p-4 sm:p-6 bg-gray-100 min-h-screen relative">
+    <div
+      className={`academic-progress h-full w-full p-2 sm:p-6 bg-gray-100 min-h-screen relative ${
+        showLogoutDialog ? "opacity-50 pointer-events-none" : ""
+      }`}
+      style={{ zIndex: showLogoutDialog ? 10 : 1 }} // Lower z-index when the logout dialog is visible
+    >
       {/* Header */}
-      <header className="flex justify-between items-center bg-blue-400 p-2 rounded mb-6 fixed top-0 w-full z-10">
+      <header className="flex justify-between items-center bg-blue-400 p-2 rounded mb-6 fixed top-0 w-full">
         <div>
           <h1 className="text-xl font-bold text-black">DASHBOARD</h1>
           <p className="text-sm text-white">Today is: {formatDate()}</p>
         </div>
-        <button onClick={onClose} className="text-white text-lg font-bold">
-          ✖
-        </button>
       </header>
 
       {/* Tab Navigation */}
       <div className="mt-20 p-4">
+        <button
+          className="absolute top-20 right-4 w-5 h-5 bg-transparent rounded-full flex items-center justify-center"
+          onClick={onClose}
+        >
+          <img src={CloseIcon} alt="Close" className="w-full h-full" />
+        </button>
+
         <div className="flex justify-around space-x-4 bg-white shadow rounded-lg p-2">
           <button
             onClick={() => setActiveTab("analytics")}
